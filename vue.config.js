c3bfa1329e1d95f0e7a7f943bf5c264c51ee216e
sewonkimm <swon962@gmail.com>
2021-01-30 21:53:39 +0900

    feat: 🍎 pwa 적용
    - pwa 적용을 위한 firebase hosting
    - menifest와 serviceworker 적용

--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,26 @@ module.exports = {
         return options;
       });
   },
+  pwa: {
+    name: "pwa",
+    short_name: "pwa",
+    icons: [
+      {
+        "src": "/img/icons/192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "/img/icons/512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ],
+    start_url: "/index.html",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#4DBA87",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+  }
 };
